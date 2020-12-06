93d1f6118c2b245b3aa8ce1e3abb1248a517840c
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-12-06 12:05:09 +0800

    项目工程创建

---
+++ demo-test0218/src/test/java/com/atguigu/test0218/demotest0218/DemoTest0218ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.test0218.demotest0218;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTest0218ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
